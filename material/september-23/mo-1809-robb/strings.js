5ff5cf1fd5c2e9e1bdf40e3a2ed594f465cfdfdb
robbdouglas <robert.nimmrich@icloud.com>
2023-09-17 20:15:08 +0200

    add tasks mo-1809-robb

---
+++ material/september-23/mo-1809-robb/strings.js
@@ -0,0 +1,20 @@
+// AUFGABE - STRINGS
+
+
+// ### Aufgabe: Verwende die oben genannten Funktionen, um die folgenden Aufgaben zu lösen:
+
+// Aufgabe 1: Berechne die Länge des folgenden Satzes und gib sie in der Konsole aus: "JavaScript ist eine mächtige Programmiersprache."
+
+// Aufgabe 2: Ändere den ersten Satz in Großbuchstaben und den zweiten Satz in Kleinbuchstaben um und gib beide in der Konsole aus:
+
+// "Hallo, wie geht es dir?"
+// "Gut, danke!"
+
+// Aufgabe 3: Extrahiere den Substring "mächtige" aus dem Satz "JavaScript ist eine mächtige Programmiersprache." und gib ihn in der Konsole aus.
+
+// Aufgabe 4: Ersetze alle Vorkommen von "Apfel" durch "Banane" in dem Satz "Ich esse gerne Äpfel und trinke Apfelsaft." und gib den modifizierten Satz in der Konsole aus.
+
+// Aufgabe 5: Teile den Satz "Die Sonne scheint am Himmel" in ein Array von Wörtern auf und gib das Array in der Konsole aus.
+
+// Aufgabe 6: Entferne alle Leerzeichen aus dem Satz " Dies ist ein Satz mit Leerzeichen. " und gib den bereinigten Satz in der Konsole aus.
+
