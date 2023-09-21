994a89bf3e1bab298469fcba01f54a831af8e922
AxelH97 <hilde.axel@web.de>
2023-09-21 09:04:52 +0200

    loesung.js hinzugefügt

---
+++ material/september-23/do-21.09-axel/loesung.js
@@ -0,0 +1,62 @@
+//1
+const number =[5, 10, 15, 20, 25];
+
+console.log(number);
+//2
+number.push(30);
+// newNumber = number.push(30);
+console.log(number);
+// console.log (newNumber)
+
+//3
+
+number.shift();
+
+console.log(number)
+
+//4
+
+const fruechte = ['Apfel','Banane','Kirsche'];
+
+console.log(fruechte)
+
+//5
+
+fruechte.push("Dattel");
+
+console.log(fruechte);
+
+//6
+
+fruechte.pop();
+
+console.log(fruechte)
+
+//7
+
+const kombiniert = number.concat(fruechte ,/* "animals","hund" */ )
+
+console.log(kombiniert);
+
+//8
+
+kombiniert.reverse();
+
+console.log(kombiniert);
+
+//9
+
+kombiniert.sort();
+
+console.log(kombiniert);
+
+//10
+
+kombiniert.join(", ");
+
+console.log(kombiniert);
+
+
+
+
+
