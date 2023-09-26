902e576e0d3b90ef79bd10c09c5153ef2be2e292
AxelH97 <hilde.axel@web.de>
2023-09-26 08:32:34 +0200

    loesung readlineSync

--- material/september-23/di-2609-axel/loesung.js
+++ material/september-23/di-2609-axel/loesung.js
@@ -1,1 +1,8 @@
+const readline = require('readline-sync');
 
+let age = readline.question("Wie alt bist du?");
+
+if (age < 18) { 
+    console.log("du bist minderjährig");}
+    else if  ( age >= 18 && age < 65 ) {console.log("Du bist im erwerbsfähigen Alter");}
+    else {console.log("Du bist im Rentenalter");}
