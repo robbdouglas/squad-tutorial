f64d8eefef95d35783aa1b6b213667484df8a46f
robbdouglas <robert.nimmrich@icloud.com>
2023-10-01 18:41:39 +0200

    add tasks for 0210-robb

---
+++ material/oktober-23/mo-0210-robb/mitschriften.js
@@ -0,0 +1,13 @@
+// FUNCTION BASICS IN JAVASCRIPT
+
+// WIEDERHOLUNG
+
+// 1. Wege, eine Funktion zu definieren
+
+// 2. Unterschiede zwischen Parameter und Argument
+
+// 3. Wann endet eine Funktion?
+
+// 4. Was ist der Unterschied zwischen return und console.log?
+
+// Was wissen wir noch über Funktionen?
